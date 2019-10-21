8f9873e022f7221872c243df38c079d067730c15
Jacques Begin <jacques.andre.begin@gmail.com>
2019-10-21 19:43:05 -0400

    Uses yargs package to parse command line arguments.

--- bin/index.js
+++ bin/index.js
@@ -2,17 +2,27 @@
 
 const chalk = require("chalk");
 const boxen = require("boxen");
+const yargs = require("yargs");
 
-const greeting = chalk.white.bold("Hello!");
+const options = yargs
+.usage("Usage: -n <name>")
+.option("n", { alias: "name", describe: "Your name", type: "string", demandOption: true })
+.argv;
 
-const boxenOptions = {
-  padding: 1,
-  margin: 1,
-  borderStyle: "round",
-  borderColor: "green",
-  backgroundColor: "#555555"
-};
+const greeting = `Hello ${options.name}!`;
 
-const msgBox = boxen(greeting, boxenOptions);
+console.log(greeting);
 
-console.log(msgBox);
+// const greeting = chalk.white.bold("Hello!");
+
+// const boxenOptions = {
+//   padding: 1,
+//   margin: 1,
+//   borderStyle: "round",
+//   borderColor: "green",
+//   backgroundColor: "#555555"
+// };
+
+// const msgBox = boxen(greeting, boxenOptions);
+
+// console.log(msgBox);
