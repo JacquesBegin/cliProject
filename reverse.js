d987d01c580071ad9fe81b3d888d28a5c7562d10
Jacques Begin <jacques.andre.begin@gmail.com>
2019-10-24 23:22:39 -0400

    Setting up reverse string as a commad line function.

---
+++ reverse.js
@@ -0,0 +1,9 @@
+// Reverse strings that are passed as arguments
+
+let args = process.argv;
+for (let x = 2; x < args.length; x++) {
+  let tmp = "";
+  for (let y = 0; y < args[x].length; y++) {
+    tmp = args
+  }
+}
