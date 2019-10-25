3e5a1ac80d6aad3759361c3b67ab06791c287310
Jacques Begin <jacques.andre.begin@gmail.com>
2019-10-24 23:24:35 -0400

    Completes reverse in the command line function.

--- reverse.js
+++ reverse.js
@@ -4,6 +4,7 @@ let args = process.argv;
 for (let x = 2; x < args.length; x++) {
   let tmp = "";
   for (let y = 0; y < args[x].length; y++) {
-    tmp = args
+    tmp = args[x][y] + tmp;
   }
+  console.log(tmp);
 }
